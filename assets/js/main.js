179b53d9ebdc01ed553818ddf64aa6206aaa3564
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-03-25 17:30:46 +0900

    Tutto commentato

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,21 +13,28 @@
 // 1) Si crea un elemento vuoto (lista???) in index e in main.js lo si "punta" associandovi una variabile, mediante queryselector o simili.
 // 2) Nel ciclo for avverranno le operazioni di creazione dei sotto-elementi (li!!!), la scrittura al loro interno del dato e l'aggiunta mediante append.
 
+// *************** Costanti e variabili ***************
+// Il programma inizia con il prompt di inserimento dei dati: 
+// Numero totale di elementi (default:100)
 const   n_str           = prompt("Inserisci il numero di elementi desierati","100");
+// Numero di elementi per rigo ovvero numero di colonne (default:7)
 const   col_number_str  = prompt("Inserisci il numero di colonne desiderate","7");
 const   n               = parseInt(n_str);
 const   col_number      = parseInt(col_number_str);
+// Gap (pixel) tra gli elementi
 const   flex_gap        = 15; 
+// Variabile associata alla "ul" presente nel index.html
 let     collection      = document.getElementById("item_collection");
+// Variabile associata al "body"
 let     body_           = document.querySelector("body");
 
 body_.className = "bg-dark";
+// Creazione e posizionamento "fixed" di titolo e sottotitolo
 document.querySelector("header").innerHTML = "<h1>FizzBuzzDOM</h1> <h2>IT WAS REALLY AMAZING</h2>";
 document.querySelector("h1").className = "text-white-50 text-center";
 document.querySelector("h1").classList.add("fixed-top");
 document.querySelector("h2").className = "text-white-50 text-center";
 document.querySelector("h2").classList.add("fixed-bottom");
-
 
 // Aggiungo classi Bootstrap alla lista con diversi metodi per testarli tutti
 collection.className = "reset_list_style";
@@ -39,40 +46,52 @@ console.log(collection);
 console.log(n_str, n);
 console.log(col_number_str, col_number);
 
+// Variabile stringa che assumerà i valori durante l'esecuzione del ciclo for e li trasferirà ai singoli "li"
 let     item        = "";
 
 for (let i=1; i<=n; i++)
 {
+    // Variabile creata ad ogni ciclo ed associata all'elemento "li" del momento
     let list_item   = document.createElement("li");
+    // Variabile stringa che assumerà i valori delle classi Bootstrap di background, a seconda dei casi
     let bg_style    = "bg-primary";  
+    // Reset della variabile ad ogni ciclo
     item = "";
 
     if ((i % 3) == 0)
     {
+        // Numero divisibile per 3
         item = "Fizz";
         bg_style = "bg-success";
     }
     if ((i % 5) == 0)
     {
+        // Numero divisibile (anche o solo) per 5
         item += "Buzz";
         bg_style = "bg-danger";
     }
     if (item == "")
     {
+        // Se item coincide con stringa nulla significa che il numero non è divisibile per nessuno tra 3, 5 e 15
         item = i;
     }
     else if (item == "FizzBuzz")
     {
+        // Item = "FizzBuzz" significa che il numero è divisibile per 15
         bg_style = "bg-warning";
     }
-    console.log(i, " ", item);
+
+    // Assegnazione del dato e delle classi Bootstrap / stili all'"li" di turno
     list_item.innerText = item;
     list_item.className = "d-flex";
     list_item.classList.add("justify-content-center", "align-items-center", bg_style);
+    // La formula seguente stabilisce il valore di larghezza ed altezza (aspect-ratio=1) dei singoli elementi
     list_item.setAttribute("style",`flex-basis: calc((100% - ((${col_number}+1)*${flex_gap}px)) / ${col_number});`);
     list_item.setAttribute("style", list_item.getAttribute("style") + "aspect-ratio: 1;");
+    // L'elemento "li" di turno viane "agganciato" alla lista
     collection.append(list_item);
 }
+// Ciclo finito
 
 console.log(collection);
 
